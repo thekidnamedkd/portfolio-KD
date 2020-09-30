a44d5b61bf396387574c34d0a1c6711d39908cf6
Kevin <thekidnamedkd@gmail.com>
2020-09-30 00:28:07 -0700

    first commit, play with config

---
+++ src/config/index.js
@@ -0,0 +1,66 @@
+module.exports = {
+
+    author: "@konstantinmuenster",
+    siteTitle: "Gatsby Starter Portfolio Minimal",
+    siteShortTitle: "Kevin Robert Davis", // Used as logo text in header, footer, and splash screen
+    siteDescription: "A portfolio site for Kevin Robert Davis.",
+    siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+    siteLanguage: "en_US",
+    siteIcon: "src/content/favicon.png", // Relative to gatsby-config file
+
+    splashScreen: true, // Set this to true if you want to use the splash screen
+
+    // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+    // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantin.muenster",
+    shownArticles: 3,
+    
+    // There are icons available for the following platforms: 
+    // Medium, GitHub, LinkedIn, XING, Behance
+    socialMedia: [
+        {
+            name: "LinkedIn",
+            url: "https://www.linkedin.com/in/kevinrobertdavis"
+        },
+        {
+            name: "Github",
+            url: "https://github.com/thekidnamedkd"
+        },
+        {
+            name: "Are.na",
+            url: "https://www.are.na/kevin-davis"
+        },
+    ],
+  
+    navLinks: {
+        menu: [
+            {
+                name: "Articles",
+                url: "/#articles",
+            },
+            {
+                name: "About",
+                url: "/#about",
+            },
+            {
+                name: "Projects",
+                url: "/#projects",
+            },
+        ],
+        button: {
+            name: "Contact",
+            url: "/#contact",
+        }
+    },
+
+    footerLinks: [
+        {
+            name: "Privacy",
+            url: "/privacy"
+        },
+        {
+            name: "Imprint",
+            url: "/imprint"
+        }
+    ]
+}
