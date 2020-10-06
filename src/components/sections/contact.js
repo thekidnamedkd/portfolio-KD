1cf59a72eac3e01b13269f7d78c65dcab6c83ce3
Kevin <thekidnamedkd@gmail.com>
2020-10-06 13:50:30 -0700

    shrink button

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -87,7 +87,7 @@ const Contact = ({ content }) => {
             </a>
           </div>
         </div>
-        <Social width="9rem" padding=".75rem 1.25rem" withIcon />
+        <Social width="8rem" padding=".75rem 1rem" withIcon />
       </StyledContentWrapper>
     </StyledSection>
   )
